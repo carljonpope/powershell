a57e160fda17cccb8689240b86a9bd8462fc2846
Carl Pope <carl.pope@broadridge.com>
2021-10-28 16:00:03 +0100

    added scripts to get vnet address spaces

--- Powershell/Azure/getVnetAddressSpacestoCsv.ps1
+++ Powershell/Azure/getVnetAddressSpacestoCsv.ps1
@@ -15,24 +15,3 @@ foreach ($vnet in $vnets) {
 
 
 }
-<#
-
-#$vnet = get-azvirtualnetwork -Name vnet1
-#$addrSpace = $vnet.AddressSpace | Select-Object @{ n='range'; e={$_.AddressPrefixes -join ' ' } }
-
-#$addrspace.range.split()
-
-foreach ($vnet in $vnets) {
-
-    $row = New-Object psobject
-    $row | Add-Member -MemberType NoteProperty -name "VNetName" -value $vnet.name
-
-    $addrSpace = $vnet.AddressSpace | Select-Object @{ n='range'; e={$_.AddressPrefixes -join ';' } }
-    $row | Add-Member -MemberType NoteProperty -name "addrSpace" -value $addrSpace.range.split()
-
-
-    Export-Csv C:\temp\testVnets.csv -Append -InputObject $row
-
-
-
-}#>
