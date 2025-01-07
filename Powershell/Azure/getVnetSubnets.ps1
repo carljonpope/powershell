f795adb8997fc5973d80779c3b08e0580f5d4dc1
Carl Pope <49517856+carljonpope@users.noreply.github.com>
2025-01-07 19:54:22 +0000

    Update getVnetSubnets.ps1

--- Powershell/Azure/getVnetSubnets.ps1
+++ Powershell/Azure/getVnetSubnets.ps1
@@ -1,7 +1,7 @@
 #Get-AzVirtualNetwork -Name vnet1 -ResourceGroupName networking | Get-AzVirtualNetworkSubnetConfig | Format-Table
 
 $vn = Get-AzVirtualNetwork -Name prod2-global-vnet01
-$vn.Subnets | Select  Name, AddressPrefix <# Name ,AddressPrefix ,NetworkSecurityGroup , ServiceEndpoints #> | Out-File c:\temp\brprdsubnets.txt
+$vn.Subnets | Select  Name, AddressPrefix <# Name ,AddressPrefix ,NetworkSecurityGroup , ServiceEndpoints #> | Out-File c:\temp\prdsubnets.txt
 $vn.AddressSpace | select Name, AddressPrefixes | Out-File c:\temp\brprdaddrspace.txt
 #$vn.Subnets | Select  Name, NetworkSecurityGroup | Out-File c:\temp\brdevnsg.txt
 
