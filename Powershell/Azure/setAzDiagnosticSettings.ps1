8c9da9a28f7a2c985b992e9a926c79b0864ede7e
Carl Pope <49517856+carljonpope@users.noreply.github.com>
2025-01-07 20:02:59 +0000

    Update setAzDiagnosticSettings.ps1

--- Powershell/Azure/setAzDiagnosticSettings.ps1
+++ Powershell/Azure/setAzDiagnosticSettings.ps1
@@ -1,19 +1,19 @@
 #$webapps = Get-AzWebApp
 
 #foreach ($webapp in $webapps) {
-$webapp = get-azwebapp -ResourceGroupName prod1-identity-rg -name prod1-identity-wa01
+$webapp = get-azwebapp -ResourceGroupName prod1-identity-rg -name prod1-xxxx-wa01
 
    $appID = $webapp.Id
    $diagSettingName = ($webapp.name) + "-diag"
 
- #  Remove-AzDiagnosticSetting -resourceid $appId -name "test-static-sites-wa01"
+ #  Remove-AzDiagnosticSetting -resourceid $appId -name "test-xxxx-sites-wa01"
 
 Set-AzDiagnosticSetting `
 -resourceid $appId `
 -name $diagSettingName `
--StorageAccountId /subscriptions/eba801d3-f1de-4a28-b172-ee8880918ce0/resourceGroups/prod1-siemintegration-rg/providers/Microsoft.Storage/storageAccounts/prod1diaglogsarchive `
+-StorageAccountId /subscriptions/exxxx-xx-4xxx28-xxx72-ee8xxx918ce0/resourceGroups/prod1-siemintegration-rg/providers/Microsoft.Storage/storageAccounts/prod1diaglogsarchive `
 -EventHubName prod1-siemintegration-eh01 `
--EventHubAuthorizationRuleId /subscriptions/eba801d3-f1de-4a28-b172-ee8880918ce0/resourceGroups/prod1-siemintegration-rg/providers/Microsoft.EventHub/namespaces/fcssiemintprod01/authorizationRules/RootManageSharedAccessKey `
+-EventHubAuthorizationRuleId /subscriptions/exxxx3-fxxe-4xxx28-xxx72-xxxx0/resourceGroups/prod1-siemintegration-rg/providers/Microsoft.EventHub/namespaces/fcssiemintprod01/authorizationRules/RootManageSharedAccessKey `
 -Enabled $true `
 -RetentionEnabled $true `
 -RetentionInDays 365 `
